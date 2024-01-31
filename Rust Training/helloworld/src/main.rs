68fb61cec737de515a3d284d2de8b6d77083e4c6
MeetButani-Inventyv <meetbutaniinventyv@gmail.com>
2024-01-31 18:27:31 +0530

    Added basic helloworld

---
+++ Rust Training/helloworld/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let a = 12;
+    let b = 4.5;
+    let c = true;
+    let d = "Welcome to Inventyv!";
+    let e = String::from("Hello world!");
+    let f = [1, 2, 3, 4, 5];
+
+    println!("{}", a);
+    println!("{}", b);
+    println!("{}", c);
+    println!("{}", d);
+    println!("{}", e);
+    println!("{:?}", f);
+}
