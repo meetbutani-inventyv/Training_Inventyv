a7dd63e47368abde4e7c73f92ae7424cf4025502
MeetButani-Inventyv <meetbutaniinventyv@gmail.com>
2024-02-08 18:32:01 +0530

    Combined string tasks 1 and 2

--- Rust Training/rust_modules/src/string_task/mod.rs
+++ Rust Training/rust_modules/src/string_task/mod.rs
@@ -10,4 +10,30 @@ pub fn main() {
 
     println!("Task-1 output:\n{}", t1);
     println!("\nTask-2 output:\n {:?}", t2);
+
+
+    // Creating domain array to store the letters that can be filled
+    let mut dom:Vec<char> = Vec::new();
+    let mut ans = String::new();
+    let mut x = 0;
+
+    for i in 0..t2.len() {
+        for _j in 0..t2[i].1 {
+            dom.push(t2[i].0);
+        }
+    }
+
+
+    // Filling the missing letters in task1 with letters from the domain array
+    for i in t1.chars() {
+        if i=='_' && dom.len()>=x+1 {
+            ans.push(dom[x]);
+            x+=1;
+        }
+        else {
+            ans.push(i);
+        }
+    }
+
+    println!("\nCombined output:\n{}", ans);
 }
