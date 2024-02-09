e4e9b054364980949d45e5dab24c671e038235e7
MeetButani-Inventyv <meetbutaniinventyv@gmail.com>
2024-02-09 18:24:00 +0530

    Removed targets folder

--- Rust Training/rust_modules/target/debug/deps/rust_modules-bb2c1a1dafc3e19b.d
+++
@@ -1,5 +0,0 @@
-G:\Inventyv\Training_Inventyv\Rust Training\rust_modules\target\debug\deps\librust_modules-bb2c1a1dafc3e19b.rmeta: src\main.rs
-
-G:\Inventyv\Training_Inventyv\Rust Training\rust_modules\target\debug\deps\rust_modules-bb2c1a1dafc3e19b.d: src\main.rs
-
-src\main.rs:
--- Rust Training/rust_modules/target/debug/deps/rust_modules-cc6a97fe7cf6da84.d
+++
@@ -1,5 +0,0 @@
-G:\Inventyv\Training_Inventyv\Rust Training\rust_modules\target\debug\deps\librust_modules-cc6a97fe7cf6da84.rmeta: src\main.rs
-
-G:\Inventyv\Training_Inventyv\Rust Training\rust_modules\target\debug\deps\rust_modules-cc6a97fe7cf6da84.d: src\main.rs
-
-src\main.rs:
